8a99445d2ee2dce72276a9e78f10100c3653c79e
Sairajnaidu5525 <83781956+Sairajnaidu5525@users.noreply.github.com>
2021-05-07 00:30:11 +0530

    Add files via upload

---
+++ box/footer.php
@@ -0,0 +1,11 @@
+<!--Footer Starts Here-->
+        <footer class="footer">
+            <div class="wrapper">
+                <p>
+                    <a href="https://www.youtube.com/vijaythapa?sub_confirmation=1/" title="ABC Institutes">ABC Institute</a> &copy; <?php echo date('Y'); ?>. All Rights Reserved.    
+                    Powered by <a href="https://www.youtube.com/vijaythapa?sub_confirmation=1" title="Vijay Thapa" target="_blank">Vijay Thapa</a>.
+                </p>
+            </div>
+        </footer>
+        <!--Footer Ends Here-->
+</html>
---
+++ box/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <!--Meta Tags Starts Here-->
+        <meta charset="UTF-8" />
+        <meta name="author" content="Beyond Boundaries" />
+        <meta name="description" content="Test Preparation Portal For Beyond Boundaries." />
+        <meta name="keywords" content="Test Preaparation, Beyond Boundaries, Online Portal, Kathmandu, Nepal" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <!--Meta Tags Ends Here-->
+        <title>Online Test Preparation Portal - Beyond Boundaries</title>
+        
+        <!--COUNT DOWN TIMER STARTS HERE-->
+        <script src="<?php echo SITEURL; ?>/assets/js/countdown/jquery.js"></script>
+        <script src="<?php echo SITEURL; ?>/assets/js/countdown/jquery.simple.timer.js"></script>
+        <script>
+          $(function(){
+            $('.timer').startTimer();
+          });
+        </script>
+        <!--COUNT DOWN TIMER ENDS HERE-->
+        
+        <!--ADDING CKEDITOR HERE-->
+        <script type="text/javascript" src="<?php echo SITEURL; ?>/assets/ckeditor/ckeditor.js"></script>
+        
+        <!--CSS File Starts Here-->
+        <link rel="stylesheet" type="text/css" href="<?php echo SITEURL; ?>/assets/css/style.css" />
+        <!--CSS File Ends Here-->
+    </head>
+    
+    <body>
+        <!--Header Starts Here-->
+        <header class="header">
+            <div class="wrapper clearfix">
+                <div class="logo">
+                    <img src="<?php echo SITEURL; ?>/images/logo.jpg" alt="Beyond Boundaries" title="Beyond Boundaries" />
+                </div>
+                
+                <div class="head-title">
+                    <h1>Online Test Preparation Portal</h1>
+                </div>
+            </div>
+        </header>
+        <!--Header Ends Here-->
